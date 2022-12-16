2cd748633e234063f36eed02ab2ee7af00d9076f
Eric Reed <mail@ericreed.me>
2022-12-15 23:45:11 -0500

    Functionality

---
+++ pages/api/aboveBelow.ts
@@ -0,0 +1,28 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+
+
+export default function handler(  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+
+  const body = req.body  
+  
+  let comp = req.body.comparator;
+  let above = 0;
+  let below = 0;
+
+  //Spec doesn't say what to do if equal, intentionally ignoring case and asking about it
+  body.numbers.forEach((num: number) => {
+    if(num > comp){
+      above++
+    } else if (num < comp){
+      below++
+    }
+  }) 
+
+  console.log('a: ', above, ' b: ', below)
+
+  res.status(200).json({ above: above, below: below })
+}
---
+++ pages/api/stringRotation.ts
@@ -0,0 +1,18 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+
+
+export default function handler(  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+
+  const body = req.body  
+
+  let initString = body.stringToRot;
+  let modRotAount  = initString.length - (body.rotateAmount % initString.length)
+
+  let stringRes = initString.slice(modRotAount) + initString.slice(0, modRotAount)  
+
+  res.status(200).json({ stringRes: stringRes })
+}
