5bd9d51da963eb422a3c4f58a64920b2393744c1
Eric Reed <mail@ericreed.me>
2022-12-15 23:45:57 -0500

    cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
-
 
 const handleAboveBelowSubmit = async (event: any) => {
     event.preventDefault()
@@ -116,8 +113,6 @@ export default function Home() {
             </main>
 
             <footer className={styles.footer}>
-          
-
                 A tiny project for RTS Labs
             </footer>
         </div>
